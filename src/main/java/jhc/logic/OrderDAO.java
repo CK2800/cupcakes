99842532d5a0d51787b21b635d641a1583467f10
Jesper Petersen <info@ordklar.dk>
2018-09-28 13:41:49 +0200

    bingo clausgit add -A!

--- src/main/java/jhc/logic/OrderDAO.java
+++ src/main/java/jhc/logic/OrderDAO.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -27,9 +28,13 @@ public class OrderDAO
     private static final String GET_USER_ORDERS_SQL = "SELECT id FROM orders WHERE userId = ?;";
     private static Connection connection;
     
-    public static ArrayList<OrderDTO> getUserOrders(UserDTO user)
-    {
-        
+//    public static ArrayList<OrderDTO> getUserOrders(UserDTO user)
+//    {
+//        
+//    }
+    
+    public static void main(String[] args) {
+        createOrder(new UserDTO(1, "Jesper", "Petersen", 0), new ArrayList());
     }
     
     public static boolean createOrder(UserDTO user, ArrayList<LineItemDTO> lineItems)
@@ -44,8 +49,11 @@ public class OrderDAO
             // create order.
              pstm.executeUpdate();
              ResultSet rs = pstm.getGeneratedKeys();
+             rs.next();
             //Q&D
-            int orderId = rs.getInt("id");
+            
+            int orderId = rs.getInt(1);
+            
             
             for(LineItemDTO lineItem : lineItems)
             {
