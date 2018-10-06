8ef0e190fe47f84df9b1516772d7ea74294fb959
CK2800 <cph-ck83@cphbusiness.dk>
2018-10-06 20:38:54 +0200

    Db layout changed, tables for toppings and bottoms created.

--- sql/data i cupcakes.sql
+++ sql/data i cupcakes.sql
@@ -1,31 +1,28 @@
--- product types
-INSERT INTO producttypes(type) VALUES('Bottom');
-INSERT INTO producttypes(type) VALUES('Topping');
 -- users
 INSERT INTO users(username, password, balance) VALUES ('Jesper', 'Petersen', 100);
 INSERT INTO users(username, password, balance) VALUES ('Henrik', 'Agger', 100);
 INSERT INTO users(username, password, balance) VALUES ('Claus', 'Kramath', 100);
 -- bottoms
-INSERT INTO products(producttypeId, name, price) VALUES (1, 'Chocolate', 5);
-INSERT INTO products(producttypeId, name, price) VALUES (1, 'Vanilla', 5);
-INSERT INTO products(producttypeId, name, price) VALUES (1, 'Nutmeg', 5);
-INSERT INTO products(producttypeId, name, price) VALUES (1, 'Pistacio', 6);
-INSERT INTO products(producttypeId, name, price) VALUES (1, 'Almond', 7);
+INSERT INTO bottoms(name, price) VALUES ('Chocolate', 5);
+INSERT INTO bottoms(name, price) VALUES ('Vanilla', 5);
+INSERT INTO bottoms(name, price) VALUES ('Nutmeg', 5);
+INSERT INTO bottoms(name, price) VALUES ('Pistacio', 6);
+INSERT INTO bottoms(name, price) VALUES ('Almond', 7);
 -- toppings
-INSERT INTO products(producttypeId, name, price) VALUES (2, 'Chocolate', 5);
-INSERT INTO products(producttypeId, name, price) VALUES (2, 'Blueberry', 5);
-INSERT INTO products(producttypeId, name, price) VALUES (2, 'Raspberry', 5);
-INSERT INTO products(producttypeId, name, price) VALUES (2, 'Crispy', 6);
-INSERT INTO products(producttypeId, name, price) VALUES (2, 'Strawberry', 6);
-INSERT INTO products(producttypeId, name, price) VALUES (2, 'Rum/Raisin', 7);
-INSERT INTO products(producttypeId, name, price) VALUES (2, 'Orange', 8);
-INSERT INTO products(producttypeId, name, price) VALUES (2, 'Lemon', 8);
-INSERT INTO products(producttypeId, name, price) VALUES (2, 'Blue cheese', 9);
+INSERT INTO toppings(name, price) VALUES ('Chocolate', 5);
+INSERT INTO toppings(name, price) VALUES ('Blueberry', 5);
+INSERT INTO toppings(name, price) VALUES ('Raspberry', 5);
+INSERT INTO toppings(name, price) VALUES ('Crispy', 6);
+INSERT INTO toppings(name, price) VALUES ('Strawberry', 6);
+INSERT INTO toppings(name, price) VALUES ('Rum/Raisin', 7);
+INSERT INTO toppings(name, price) VALUES ('Orange', 8);
+INSERT INTO toppings(name, price) VALUES ('Lemon', 8);
+INSERT INTO toppings(name, price) VALUES ('Blue cheese', 9);
 -- orders
 INSERT INTO orders(userId) VALUES(1);
 INSERT INTO orders(userId) VALUES(2);
 INSERT INTO orders(userId) VALUES(3);
 -- line items, order 1
-INSERT INTO lineitems(orderId, productId, qty, price) VALUES(1, 1, 2, 10);
-INSERT INTO lineitems(orderId, productId, qty, price) VALUES(1, 7, 1, 5);
-INSERT INTO lineitems(orderId, productId, qty, price) VALUES(1, 9, 1, 6);
+INSERT INTO lineitems(orderId, toppingId, bottomId, qty, price) VALUES(1, 1, 2, 2, 10);
+INSERT INTO lineitems(orderId, toppingId, bottomId, qty, price) VALUES(1, 4, 3, 1, 12);
+INSERT INTO lineitems(orderId, toppingId, bottomId, qty, price) VALUES(1, 5, 8, 1, 15);
--- sql/oprettelse af cupcake db.sql
+++ sql/oprettelse af cupcake db.sql
@@ -6,13 +6,11 @@ USE cupcakes;
 DROP TABLE IF EXISTS lineitems;
 DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS bottoms;
+DROP TABLE IF EXISTS toppings;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS producttypes;
 
-CREATE TABLE producttypes(
-id int PRIMARY KEY AUTO_INCREMENT,
-`type` VARCHAR(50) NOT NULL
-);
 
 CREATE TABLE users(
 id int PRIMARY KEY AUTO_INCREMENT,
@@ -21,15 +19,16 @@ username VARCHAR(30) NOT NULL UNIQUE,
 balance DECIMAL(13,2) NOT NULL DEFAULT 0
 );
 
-CREATE TABLE products(
+CREATE TABLE bottoms(
 id int PRIMARY KEY AUTO_INCREMENT,
-producttypeId int NOT NULL,
-`name` VARCHAR(50) NOT NULL,
-price DECIMAL(13,2) NOT NULL,
-CONSTRAINT fk_products_producttypes
-FOREIGN KEY (producttypeId)
-REFERENCES producttypes(id)
-ON DELETE NO ACTION -- producttypes referenced by products can not be deleted.
+name VARCHAR(50) NOT NULL,
+price DECIMAL(13,2) NOT NULL
+);
+
+CREATE TABLE toppings(
+id int PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50) NOT NULL,
+price DECIMAL(13,2) NOT NULL
 );
 
 CREATE TABLE orders(
@@ -42,20 +41,24 @@ ON DELETE NO ACTION -- users referenced by orders can not be deleted.
 );
 
 CREATE TABLE lineitems(
--- id int PRIMARY KEY AUTO_INCREMENT,
 orderId int,
-productId int,
+toppingId int,
+bottomId int,
 qty int NOT NULL,
 price DECIMAL(13,2) NOT NULL,
-PRIMARY KEY(orderId, productId), -- sammensat primær ngl. istedet for (godt set, Jesper).
+PRIMARY KEY(orderId, toppingId, bottomId), -- sammensat primær ngl. istedet for (godt set, Jesper).
 CONSTRAINT fk_lineitems_orders
 FOREIGN KEY(orderId)
 REFERENCES orders(id)
 ON DELETE CASCADE, -- if an order is deleted, so are its lineitems.
-CONSTRAINT fk_lineitems_products
-FOREIGN KEY(productId)
-REFERENCES products(id)
-ON DELETE NO ACTION -- products referenced by lineitems can not be deleted.
+CONSTRAINT fk_lineitems_toppings
+FOREIGN KEY(toppingId)
+REFERENCES toppings(id)
+ON DELETE NO ACTION, -- toppings referenced by lineitems can not be deleted.
+CONSTRAINT fk_lineitems_bottoms
+FOREIGN KEY(bottomId)
+REFERENCES toppings(id)
+ON DELETE NO ACTION -- bottoms referenced by lineitems can not be deleted.
 );
 
 
